63f34686b24f85d51730c17c31bc972c2c5d4b46
Abraomukas <abraomukas@gmail.com>
2023-07-04 23:10:39 +0400

    feat: New structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<App />} />
+				<Route></Route>
+				<Route></Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
